e302f3464769bc663f804f5d28e928551974ab4f
Matej Bačo <matejbaco2000@gmail.com>
2023-12-09 13:24:15 +0100

    Simplify

--- src/tools/faker/services/storage.js
+++ src/tools/faker/services/storage.js
@@ -149,7 +149,7 @@ async function generateFiles(appwrite, buckets) {
     const bucket = buckets[i];
 
     for (let r = 0; r < filesNo; r += 1) {
-      const fileURL = getFakeFileURL();
+      const fileURL = 'https://picsum.photos/seed/appwrite/200/200';
 
       await streamUploadFromURL(fileURL, storageClient, bucket.$id).then(
         (response) => {
